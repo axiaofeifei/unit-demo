70b5b89076f491cda14866e348da43e1b2a1d6cb
hhf <2856554735@qq.com>
2022-06-02 11:35:15 +0800

    数据表文件

--- sql/student.sql
+++ sql/student.sql
@@ -52,24 +52,4 @@ INSERT INTO `student` VALUES ('18', '2020018', 'mary', '23', '女', '2020-05-20'
 INSERT INTO `student` VALUES ('19', '2020019', 'sara', '54', '男', '2020-06-17');
 INSERT INTO `student` VALUES ('20', '2020020', 'Naruto', '32', '男', '2020-11-11');
 INSERT INTO `student` VALUES ('21', '2022444', '李伟', '63', '男', '2020-09-23');
-INSERT INTO `student` VALUES ('22', '2020423', '孙晓峰', '25', '男', '2020-09-02');
-INSERT INTO `student` VALUES ('23', '2020075', '王云腾', '21', '男', '2020-03-20');
-INSERT INTO `student` VALUES ('24', '2020254', '黄诗芮', '22', '男', '2020-07-26');
-INSERT INTO `student` VALUES ('25', '2020755', '刘晓龙', '23', '女', '2020-05-20');
-INSERT INTO `student` VALUES ('26', '2020424', '苏寿德', '30', '男', '2020-06-17');
-INSERT INTO `student` VALUES ('27', '2020027', '徐乐程', '25', '男', '2020-11-11');
-INSERT INTO `student` VALUES ('28', '2020028', '郭宇鹏', '32', '男', '2020-07-26');
-INSERT INTO `student` VALUES ('29', '2020029', '王丽燕', '23', '女', '2020-05-20');
-INSERT INTO `student` VALUES ('30', '2020030', '李佳俊', '23', '男', '2020-06-17');
-INSERT INTO `student` VALUES ('31', '2020031', '沈景新', '21', '男', '2020-11-01');
-INSERT INTO `student` VALUES ('32', '2020032', '王宸', '24', '男', '2020-10-11');
-INSERT INTO `student` VALUES ('33', '2020033', '郭志方', '24', '男', '2020-11-11');
-INSERT INTO `student` VALUES ('34', '2020034', '杨倩', '25', '女', '2020-12-11');
-INSERT INTO `student` VALUES ('35', '2020035', '魏珂', '23', '男', '2020-01-11');
-INSERT INTO `student` VALUES ('36', '2020036', '李庆云', '27', '女', '2020-01-21');
-INSERT INTO `student` VALUES ('37', '2020037', '王培', '28', '男', '2020-11-17');
-INSERT INTO `student` VALUES ('38', '2020038', '路峰峰', '24', '女', '2020-11-01');
-INSERT INTO `student` VALUES ('39', '2020039', '陈晶', '24', '女', '2020-01-21');
-INSERT INTO `student` VALUES ('40', '2020040', '腾利', '26', '男', '2020-11-17');
-INSERT INTO `student` VALUES ('41', '2020041', '繆庆瑞', '21', '男', '2020-11-12');
-INSERT INTO `student` VALUES ('42', '2020042', '乔莉', '24', '女', '2020-11-14');
+
